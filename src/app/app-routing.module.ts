c999e9fbedbe044702a877bc821825a1ebfa1873
Lars Juul <ljb@easv.dk>
2021-01-27 09:33:40 +0100

    Adding Chat frontend

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
-  { path: 'chats', loadChildren: () => import('./chat/chat.module').then(m => m.ChatModule) }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
