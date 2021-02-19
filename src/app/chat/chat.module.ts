be1896e1c611bf6be78eb3d492a01ae1790f332a
Lars Juul <ljb@easv.dk>
2021-02-19 11:18:55 +0100

    Adding Material

--- src/app/chat/chat.module.ts
+++ src/app/chat/chat.module.ts
@@ -4,13 +4,15 @@ import { CommonModule } from '@angular/common';
 import { ChatRoutingModule } from './chat-routing.module';
 import { ChatComponent } from './chat.component';
 import {ReactiveFormsModule} from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [ChatComponent],
   imports: [
     CommonModule,
     ChatRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatButtonModule
   ]
 })
 export class ChatModule { }
